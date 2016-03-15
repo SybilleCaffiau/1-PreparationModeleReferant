02cb35f365e0b887d76712328eecf6c015df9cae
SybilleCaffiau <sybille.caffiau@imag.fr>
2016-03-15 12:44:52 +0100

    production label après replace
    après remplacement, le label de sortie est le meme que celui produit
    par la transition initiale
    Correction de l’issue #4

--- Traitement/trans.cpp
+++ Traitement/trans.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[]){
 	string automateN = argv[2];
 	string automateN1= argv[1];
 	int l=atoi(argv[3]);
+	int lprod;
 
 	//Récupération des automates fst à traiter n= niveau le plus bas et n1 = niveau plus haut
 	StdVectorFst *n =StdVectorFst::Read(automateN);
@@ -17,29 +18,7 @@ int main(int argc, char *argv[]){
 	
 	StdVectorFst f;
 
-	//Pour tous les états de niveau n
-	for (StateIterator<StdFst> siter(*n); !siter.Done(); siter.Next()) { 
-		f.AddState();
-		
-  		StdArc arc;
- 		// Iterates over state i's arcs. 
-		for (ArcIterator<StdFst> aiter(*n, siter.Value()); !aiter.Done(); aiter.Next()){
-  			 arc = aiter.Value();
-  			 if(arc.olabel==l){
-  			 	//cout << "dans trouve" <<endl;
-  			 	arc.olabel = arc.ilabel;
-  			 	//aiter.SetValue(arc);
-  			 }
-  			 //arc.olabel = arc.ilabel;
-  			 f.AddArc(siter.Value(), arc);
-  			 cout << "un nouvel arc avec " << arc.olabel << endl;
-		}
-	//arbitrairement nous plaçons l'état de départ sur 0 et l'état final à 0 
-     	f.SetStart(0);
-     	
-	}
-    f.SetFinal(0, 0);
-    //Pour tous les états de niveau n1
+	 //Pour tous les états de niveau n1
     for (StateIterator<StdFst> siter(*n1); !siter.Done(); siter.Next()) {
     	//pour tous les input des transitions qui partent de l'état
 		Matcher<StdFst> matcher(*n1, MATCH_INPUT);
@@ -51,10 +30,35 @@ int main(int argc, char *argv[]){
      				//L'arc de l'automate consomme l
 			    	//on cree une transition en inversant les labels
 			    	//(dep, arc(labelin,labelout,poid,fin)
+			    	lprod=matcher.Value().olabel;
 			   		n1->AddArc(0, StdArc(matcher.Value().olabel, l, 1, 0));
   				}
 	}
      
+
+	//Pour tous les états de niveau n
+	for (StateIterator<StdFst> siter(*n); !siter.Done(); siter.Next()) { 
+		f.AddState();
+		
+  		StdArc arc;
+ 		// Iterates over state i's arcs. 
+		for (ArcIterator<StdFst> aiter(*n, siter.Value()); !aiter.Done(); aiter.Next()){
+  			 arc = aiter.Value();
+  			 if(arc.olabel==l){
+  			 	//cout << "dans trouve" <<endl;
+  			 	arc.olabel = lprod;
+  			 	//aiter.SetValue(arc);
+  			 }
+  			 //arc.olabel = arc.ilabel;
+  			 f.AddArc(siter.Value(), arc);
+  			 cout << "un nouvel arc avec " << arc.olabel << endl;
+		}
+	//arbitrairement nous plaçons l'état de départ sur 0 et l'état final à 0 
+     	f.SetStart(0);
+     	
+	}
+    f.SetFinal(0, 0);
+   
     
      
 	//n->Write("ntest.fst");
